6232c4eb7a31e73b75bf8fe9d03dfa69ac1b2600
ramez <rhanna@iotblue.net>
2022-02-24 13:33:22 +0200

    fix build

--- src/emqx_bridge_nats.erl
+++ src/emqx_bridge_nats.erl
@@ -149,7 +149,7 @@ publish_to_nats(Message, Topic) ->
     Payload = emqx_json:encode(Message),
     io:format("Payload: ~p~n", [Payload]),
     nats:pub(Conn, Topic, #{payload => Payload}),
-    {ok, Message}.
+    ok.
 
 format_payload(Message, Action) ->
     <<T1:64, T2:48, T3:16>> = Message#message.id,
